8db960d42b3236a76379b1943f8780d8a718dc4c
prettydy <prettydt@163.com>
2019-11-28 17:07:33 +0800

    first Commit

---
+++ A11/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  A11
+//
+//  Created by Guoliang Li on 2019-11-28.
+//  Copyright © 2019 Guoliang Li. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+var aa = ""
+var bb = aa + ":"
+print("fafa")
+print("fa222")
+
+
+func name() -> String {
+    return "returnString"
+    print("afa")
+}
+var n = name()
+print("\(n)")
